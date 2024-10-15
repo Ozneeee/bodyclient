6829643cfc4a4dd0ee96c7d5a0f64baec807304e
Ozne <xxtagashixx@gmail.com>
2024-10-15 16:15:20 +0200

    first commit

---
+++ bodyclient/src/main/java/com/enzo/bodyclient/BodyclientApplication.java
@@ -0,0 +1,13 @@
+package com.enzo.bodyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyclientApplication.class, args);
+	}
+
+}
---
+++ bodyclient/src/test/java/com/enzo/bodyclient/BodyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo.bodyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
